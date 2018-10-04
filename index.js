8ba307b1613adbf46f97e72adc272b05614f7892
MahmoudSaleh15 <43014605+MahmoudSaleh15@users.noreply.github.com>
2018-10-04 13:23:56 -0600

    update

--- index.js
+++ index.js
@@ -6,3 +6,22 @@ for (let i=0; i<=difference; i++){
   even = (startingNumber += skipCount) % 2;
   console.log("This number is", even, "; This iteration is number", i+skipCount); //Example of Concatenating
 }
+
+
+function startTime() {
+    var today = new Date();
+    var h = today.getHours();
+    var m = today.getMinutes();
+    var s = today.getSeconds();
+    m = checkTime(m);
+    s = checkTime(s);
+    document.getElementById('txt').innerHTML =
+    h + ":" + m + ":" + s;
+    var t = setTimeout(startTime, 500);
+}
+function checkTime(i) {
+    if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
+    return i;
+}
+
+console.log(startTime);
